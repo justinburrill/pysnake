37386624e54b2a66071a27ae5999e22d8c8182fa
Justin Burrill <me@justinburrill.ca>
2022-11-08 14:10:19 -0500

    getres fix attempt

--- snake.py
+++ snake.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import random
 import sys
-sys.path.insert(0, '../util_package/')
+sys.path.insert(0, '..y/util_package/')
 from util_package import getres
 
 # Initialize window
